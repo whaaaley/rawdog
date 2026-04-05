4b48f3ad7b6251b271e0df414ca4e3e7851af791
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2026-04-04 22:51:14 -0500

    feat(helpers): add exec result type and return promise

--- helpers/exec.ts
+++ helpers/exec.ts
@@ -1,6 +1,13 @@
-export const exec = async (cmd: string, args: string[]) => {
+type ExecResult = {
+  code: number
+  stdout: string
+  stderr: string
+}
+
+export const exec = async (cmd: string, args: string[]): Promise<ExecResult> => {
   const c = new Deno.Command(cmd, { args, stdout: 'piped', stderr: 'piped' })
   const { code, stdout, stderr } = await c.output()
+
   return {
     code,
     stdout: new TextDecoder().decode(stdout).trim(),
