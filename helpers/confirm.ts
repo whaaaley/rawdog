2e394ee6f764ed1d2b371a638136f2a15395d520
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2026-04-04 22:48:30 -0500

    fix(helpers): fix stdin read logic in confirm helper

--- helpers/confirm.ts
+++ helpers/confirm.ts
@@ -5,10 +5,10 @@ export const confirm = async (message: string): Promise<boolean> => {
   await Deno.stdout.write(encoder.encode(`${message} [Y/n] `))
 
   Deno.stdin.setRaw(true)
-  const n: number | null = await Deno.stdin.read(buf)
+  await Deno.stdin.read(buf)
   Deno.stdin.setRaw(false)
 
-  const key: number = n ? buf[0] : 0
+  const [key = 0]: Uint8Array = buf
   const rejected: boolean = key === 110 || key === 78 // n or N
 
   await Deno.stdout.write(encoder.encode(rejected ? 'n\n' : 'y\n'))
